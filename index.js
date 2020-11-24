004ec3367908d997b0cdfec63ae462f829eb9c02
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-11-24 16:45:50 -0500

    Prettier the code

--- index.js
+++ index.js
@@ -6,46 +6,45 @@ class Account {
   get balance() {
     let balance = 0;
     for (let t of this.transactions) {
-      balance += t.value
+      balance += t.value;
     }
     return balance;
   }
   addTransaction(transaction) {
-    this.transactions.push(transaction)
+    this.transactions.push(transaction);
   }
 }
 
 class Transaction {
-  constructor(amount,account) {
+  constructor(amount, account) {
     this.amount = amount;
     this.account = account;
-    console.log('this.amount:', this.amount, '!!! this account:', this.account)
+    console.log("this.amount:", this.amount, "!!! this account:", this.account);
   }
 
   commit() {
     this.time = new Date();
     this.account.addTransaction(this);
   }
-
-};
+}
 
 class Withdrawal extends Transaction {
   get value() {
-    return -this.amount
+    return -this.amount;
   }
 }
 
 class Deposit extends Transaction {
   get value() {
-    return this.amount
+    return this.amount;
   }
 }
 
 // DRIVER CODE BELOW
 const myAccount = new Account("snow-patrol");
-console.log('Starting Balance:', myAccount.balance);
-const t1 = new Deposit(120.00, myAccount);
+console.log("Starting Balance:", myAccount.balance);
+const t1 = new Deposit(120.0, myAccount);
 t1.commit();
-const t2 = new Withdrawal(50.00, myAccount);
+const t2 = new Withdrawal(50.0, myAccount);
 t2.commit();
-console.log('Ending Balance:', myAccount.balance);
+console.log("Ending Balance:", myAccount.balance);
