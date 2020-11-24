ec9375d81f08318056c32347e35727548465f82e
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-11-24 16:45:06 -0500

    Updating file

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+  get balance() {
+    let balance = 0;
+    for (let t of this.transactions) {
+      balance += t.value
+    }
+    return balance;
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction)
+  }
+}
 
-class Withdrawal {
-
-  constructor(amount) {
+class Transaction {
+  constructor(amount,account) {
     this.amount = amount;
+    this.account = account;
+    console.log('this.amount:', this.amount, '!!! this account:', this.account)
   }
 
   commit() {
-    balance -= this.amount;
+    this.time = new Date();
+    this.account.addTransaction(this);
   }
 
+};
+
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount
+  }
 }
 
-
-
+class Deposit extends Transaction {
+  get value() {
+    return this.amount
+  }
+}
 
 // DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+console.log('Starting Balance:', myAccount.balance);
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
-
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
